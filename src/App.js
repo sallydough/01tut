fe72086679cc199da50e6b17f837da006923a7fb
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-09 11:03:21 -0700

    created handleClick buttons

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import Content from './Content';
 import Footer from './Footer';
 
 function App() {
-
-  // declare all of your functions here
-  const handleNameChange = () => {
-    const names = ["Sally", "Kevin", "Myles"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
   
   return (
     <div className="App">
       <Header />
       <Content />
       <Footer />
-    
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -8,10 +8,27 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("You clicked it");
+  }
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+  const handleClick3 = (event) => {
+    // console logs event target aka event element
+    console.log(event.target);
+  }
+
   return (
     // calling your function expressions below under the Content() return statement
     <main>
-      <p> Hello {handleNameChange()} </p>
+      <p> Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>Click Here</button>
+      {/* use anonymous function "()=>" for indication of function being called only when button is clicked */}
+      <button onClick={() => handleClick2("Anita")}>Click Name</button>
+      {/* passes event in and returns event target element */}
+      <button onClick={(event) => handleClick3(event)}>Click Event</button>
+
     </main>
   );
 };
