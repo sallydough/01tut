49114f0383dd2c37a61c8ee37ff1539d26c07ae4
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-09 11:39:45 -0700

    setState count created

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
   // declare all of your functions here
+
+    // useState hook to preserve and change name
+    // name is the current state name at any given time (getter)
+    // setName is used to set the state (setter)
+    const [name, setName] = useState("Josh")
+
+    const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
-    const names = ["Sally", "Kevin", "Myles"];
+    const name = ["Sally", "Kevin", "Myles"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    // setName will be pick a name from array above and push that name into current spot of "Josh"
+    setName(name[int]);
   };
 
   const handleClick = () => {
-    console.log("You clicked it");
+    console.log(count);
+     const counting = count + 1;
+     setCount(counting);
+
   }
   const handleClick2 = (name) => {
     console.log(`${name} was clicked`);
@@ -22,8 +35,10 @@ const Content = () => {
   return (
     // calling your function expressions below under the Content() return statement
     <main>
-      <p> Hello {handleNameChange()}</p>
-      <button onClick={handleClick}>Click Here</button>
+      <p> Hello {name}</p>
+      <button onClick={handleNameChange}>setState Name Change</button>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Count Click</button>
       {/* use anonymous function "()=>" for indication of function being called only when button is clicked */}
       <button onClick={() => handleClick2("Anita")}>Click Name</button>
       {/* passes event in and returns event target element */}
