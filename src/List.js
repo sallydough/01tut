12fe25f9f433098d1dc50a0c6e7aaaac96ceb3bf
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-10 08:20:27 -0700

    added delete state button

--- src/List.js
+++ src/List.js
@@ -27,12 +27,18 @@ const List = () => {
     setItems(listItems);
     // stores the rendered information so when you close and open app again, it will pop up with all changed info
     localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
 
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
   }
 
   return (
   
   <main>
+    {items.length ? (
       <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
@@ -44,10 +50,16 @@ const List = () => {
             onDoubleClick={() => handleCheck(item.id)}
             style={(item.checked) ? {textDecoration: 'line-through'} :null }
             >{item.item}</label>
-            <FaTrashAlt role="button" taxIndex="0"/>
+            <FaTrashAlt 
+            onClick={() => handleDelete(item.id)}
+            role="button" 
+            taxIndex="0"/>
           </li>
         ))}
       </ul>
+    ): (
+      <p style={{marginTop: '2rem'}}>Your List is Empty</p>
+    )}
   </main>
   )
 };
