86b086d86cdbefe58b539710862f411ae0d6c1f2
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-10 08:09:26 -0700

    list checked function working

--- src/List.js
+++ src/List.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
+import {FaTrashAlt} from 'react-icons/fa'
 
 const List = () => {
   const [items, setItems] = useState([
     {
       id: 1,
       checked: false,
-      item: "One half pound Cocoa Covered Almonds",
+      item: "One 1/2 pound Cocoa Covered Almonds",
     },
     {
       id: 2,
@@ -14,21 +15,36 @@ const List = () => {
       item: "Item 2",
     },
     {
-      id: 1,
+      id: 3,
       checked: false,
       item: "Item 3",
     },
   ]);
+
+  const handleCheck = (id) => {
+    console.log('clicked')
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item)
+    setItems(listItems);
+    // stores the rendered information so when you close and open app again, it will pop up with all changed info
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+
+  }
 
   return (
   
   <main>
       <ul>
         {items.map((item) => (
-          <li className="item">
-            <input type="checkbox"
+          <li className="item" key={item.id}>
+            <input 
+            type="checkbox"
+            onChange={() => handleCheck(item.id)}
             checked={item.checked} />
-            <label>{item.item}</label>
+            <label
+            onDoubleClick={() => handleCheck(item.id)}
+            style={(item.checked) ? {textDecoration: 'line-through'} :null }
+            >{item.item}</label>
+            <FaTrashAlt role="button" taxIndex="0"/>
           </li>
         ))}
       </ul>
